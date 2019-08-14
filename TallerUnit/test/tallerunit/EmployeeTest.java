4e0789e93ba165424e1a3decf6ac7ba8735f2af4
Luis <luis8e@hotmail.com>
2019-08-14 11:15:08 -0500

    Pruebas para worker

--- TallerUnit/test/tallerunit/EmployeeTest.java
+++ TallerUnit/test/tallerunit/EmployeeTest.java
@@ -38,6 +38,39 @@ public class EmployeeTest  {
         Assert.assertEquals(worker.cs(),517.5,0.05);
     }
     
+     //Verifica que el metodo cs de employee calcule bien el salario para un empleado de tipo worker con currency USD
+    @Test
+    public void VerificarCsWorkerUSD(){
+        Employee emp=new Employee(200,"USD",6,EmployeeType.Worker);
+        double cs=emp.cs();
+        Assert.assertEquals(200, cs,0.1);
+    }
+    
+    //Verifica que el metodo cs de employee calcule bien el salario para un empleado de tipo worker con cualquier otro currency que no sa USD
+    @Test
+    public void VerificarCsWorker(){
+        Employee emp=new Employee(200,"",6,EmployeeType.Worker);
+        double cs=emp.cs();
+        Assert.assertEquals(190, cs,0.1);
+    }
+    
+    
+    //Verifica que el metodo CalculateYearBonus de el resultado correcto para un empleado de tipo worker con currency USD
+    @Test
+    public void VerificarYearBonusWorkerUSD(){
+        Employee emp=new Employee(200,"USD",6,EmployeeType.Worker);
+        double yb=emp.CalculateYearBonus();
+        Assert.assertEquals(3860, yb,0.1);
+    }
+    
+    //Verifica que el metodo CalculateYearBonus de el resultado correcto para un empleado de tipo worker con otro currency que no sea USD
+    @Test
+    public void VerificarYearBonusWorker(){
+        Employee emp=new Employee(200,"",6,EmployeeType.Worker);
+        double yb=emp.CalculateYearBonus();
+        Assert.assertEquals(3860, yb,0.1);
+    } 
+     
     @BeforeClass
     public static void setUpClass() {
     }
